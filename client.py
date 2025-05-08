2712251a4486f83d198365b373a06a0b28371970
DAN <daniponti35@gmail.com>
2025-05-08 08:16:07 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,31 @@
 import socket
 import subprocess
 
-SERVER_IP = '127.0.0.1'
-PORT = 9999
 
-# Crea socket e si connette al server
+SERVER_IP = '127.0.0.1'
+PORT = 9998
+
+# Crea il socket e si connette al server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_IP, PORT))
 print(f"[CLIENT] Connesso a {SERVER_IP}:{PORT}")
 
-# Avvia FFplay per riprodurre lo stream MPEG-TS ricevuto
+# Avvia FFplay per riprodurre il flusso
 ffplay = subprocess.Popen([
     'ffplay', '-i', 'pipe:0', '-fflags', 'nobuffer'
 ], stdin=subprocess.PIPE)
 
-# Legge dal socket e scrive nel player
-while True:
-    data = client_socket.recv(1024)
-    if not data:
-        break
-    ffplay.stdin.write(data)
+# Riceve dati e li scrive nello stdin di ffplay
+try:
 
-client_socket.close()
-ffplay.terminate()
-print("[CLIENT] Streaming terminato.")
+     while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        ffplay.stdin.write(data)
+except KeyboardInterrupt:
+    print("\n[CLIENT] Interrotto dall'utente.")
+finally:
+    client_socket.close()
+    ffplay.terminate()
+    print("[CLIENT] Streaming terminato.")
